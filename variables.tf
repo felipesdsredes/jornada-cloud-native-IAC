b8730cc350d66ade698902911ab5b5fac155ead5
efilippini <36900253+efilippini@users.noreply.github.com>
2023-08-16 17:10:50 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Definição de variáveis a serem utilizadas durante o processo de provisionamento
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
